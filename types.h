41abbf59f5d6f5b77a8071b9d3f44c70024ebfa4
Chris Glover <c.d.glover@gmail.com>
2016-08-13 13:21:46 -0700

    Update to fixes from type_index.

--- types.h
+++ types.h
@@ -19,83 +19,83 @@
 
 struct base {
     virtual ~base() {};
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS(BOOST_PP_SEQ_NIL)
     IMPLEMENT_CLASS(base)
 };
 
 struct single1 : base {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(base)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((base))
     IMPLEMENT_CLASS(single1)
 };
 
 struct single2 : single1 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single1)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single1))
     IMPLEMENT_CLASS(single2)
 };
 
 struct single3 : single2 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single2)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single2))
     IMPLEMENT_CLASS(single3)
 };
 
 struct single4 : single3 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single3)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single3))
     IMPLEMENT_CLASS(single4)
 };
 
 struct single5 : single4 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single4)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single4))
     IMPLEMENT_CLASS(single5)
 };
 
 struct single6 : single5 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single5)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single5))
     IMPLEMENT_CLASS(single6)
 };
 
 struct single7 : single6 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single6)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single6))
     IMPLEMENT_CLASS(single7)
 };
 
 struct single8 : single7 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single7)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single7))
     IMPLEMENT_CLASS(single8)
 };
 
 struct single9 : single8 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single8)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single8))
     IMPLEMENT_CLASS(single9)
 };
 
 struct single10 : single9 {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(single9)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((single9))
     IMPLEMENT_CLASS(single10)
 };
 
 #define MULTI_CLASS(klass, base1, base2) \
     struct klass : base1, base2 { \
-        BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(base1, base2) \
+        BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((base1)(base2)) \
         IMPLEMENT_CLASS(klass) \
     };
 
 struct base1 : virtual base {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(base)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((base))
     IMPLEMENT_CLASS(base1)
 };
 
 struct base2 : virtual base  {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(base)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((base))
     IMPLEMENT_CLASS(base2)
 };
 
 struct base3 : virtual base {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(base)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((base))
     IMPLEMENT_CLASS(base3)
 };
 
 struct base4 : virtual base {
-    BOOST_TYPE_INDEX_REGISTER_CLASS_RTTI_BASES(base)
+    BOOST_TYPE_INDEX_REGISTER_RUNTIME_CLASS((base))
     IMPLEMENT_CLASS(base4)
 };
 
